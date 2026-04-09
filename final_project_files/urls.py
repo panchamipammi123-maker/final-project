93404805ca23c6f0f551327dfa7012038cf67da4
panchamipammi123-maker <panchamipammi123@gmail.com>
2026-04-09 12:59:04 +0530

    Add files via upload

---
+++ final_project_files/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'final_project_app'
+
+urlpatterns = [
+    path('submit/<int:course_id>/', views.submit, name='submit'),
+    path('result/<int:submission_id>/', views.show_exam_result, name='show_exam_result'),
+]
