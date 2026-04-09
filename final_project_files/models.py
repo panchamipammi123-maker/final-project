4790a9f8b2c315658671b0c25dcd98d66d4fe9f7
panchamipammi123-maker <panchamipammi123@gmail.com>
2026-04-09 12:28:58 +0530

    Update models.py

--- final_project_files/models.py
+++ final_project_files/models.py
@@ -1,1 +1,21 @@
 
+
+from django.db import models
+
+# Question model
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    # add other fields if needed, e.g., course, difficulty
+
+# Choice model
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+# Submission model
+class Submission(models.Model):
+    choices = models.ManyToManyField(Choice)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
